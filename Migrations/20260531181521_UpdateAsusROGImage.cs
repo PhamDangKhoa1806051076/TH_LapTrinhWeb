b97dc072e911bb425bade1ce5597ef51b0dfca80
Nonesen <panh70573@gmail.com>
2026-06-01 01:17:04 +0700

    Update Asus ROG image: replace 80.jpg with asusrog.jpg, add migration

---
+++ Migrations/20260531181521_UpdateAsusROGImage.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PhamDangKhoa_W345_C2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateAsusROGImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/asusrog.jpg");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5,
+                column: "ImageUrl",
+                value: "/images/80.jpg");
+        }
+    }
+}
